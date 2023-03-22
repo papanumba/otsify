fa8eee9c6c76b256382a2b9cdedc2eaf92dd153b
papanumba <128253270+papanumba@users.noreply.github.com>
2023-03-22 18:45:10 +0100

    Upload all files

---
+++ README.md
@@ -0,0 +1,49 @@
+# Otsify
+**Otsify** (pron. \[ˈo̞ː.tsɪ.faɪ\]) is a simple CLI tool for otsifying images,
+(just kidding) i.e. converting any image into black&white.
+
+## Etymology
+The name is a portmanteau of the surname *Ōtsu* (see **Description**) and the
+suffix *-ify* (as in *simple* (adj) > *simplify* (v)).
+Grammatically, it is a verb, so it can be used as in "Wait a second! I haven't
+otsified this image yet.".
+
+## Description
+The program uses [Otsu's method](https://en.wikipedia.org/wiki/Otsu%27s_method)
+so as to threshold an image automatically.
+Afterwards, it also filters the "isolated" pixels, thus visually explained:
+
+```
+# = black
+· = white
+
+# # #     # # #
+# · # ==> # # #
+# # #     # # #
+
+· · ·     · · ·
+· # · ==> · · ·
+· · ·     · · ·
+```
+
+This helps shrink the output image file size, because of how the `.png` format
+works. Otsu's method leaves many of such pixels on images that either have a lot
+of noise or don't have a strongly bimodal gray histogram. So it is particularly
+helpfull on those kind of images.
+
+## TODO
+Brainstorm of new features:
+* `--colored` or `--rgb`: this flag would otsify each RGB channel individually.
+
+## Installation
+Download the binary which suits your operating system & run it from the command
+line. Otherwise, þou canst build it from source.
+
+## Usage
+Pass as CLI arguments the names of the input (I) and output (O) image files.
+* `./otsify I O` will read I, then otsify it & finally save it in O.
+* `./otsify I` will read I, otsify it & save it in a default name made from I.
+
+## License
+This software is licensed under the GPLv3.
+
---
+++ src/img.rs
@@ -0,0 +1,113 @@
+// mod img @ src/img.rs
+// mar-2022 - mar-2023
+// Calculations explained in N. Otsu 1979, IEEE
+
+
+use image::GrayImage; // uses u8 per pixel
+
+const MAXL: usize = 0x100; // 256 gray values of GrayImage
+
+
+// otsifies þe gray image
+pub fn otsify_gray(img: &mut GrayImage)
+{
+    // aux var bcoz multiple borrow of img (error E0499)
+    let max_k: u8 = get_otsu_max_k(img);
+    threshold_gray(img, max_k);
+}
+
+/*
+**  Removes isolated pixels (read þe README).
+**  Expects a b&w image, but can get any.
+**  When gets a gray one, makes it crappy,
+**  random but pretty cool, so here I leave it
+*/
+pub fn remove_isolates(img: &mut GrayImage) {
+    let (w, h) = img.dimensions();
+    for x in 1..(w-1) {
+        for y in 1..(h-1) {
+            let p: u8 = img[(x,y  )].0[0];
+            if  p == img[(x-1, y  )].0[0] || p == img[(x  , y-1)].0[0]
+             || p == img[(x+1, y  )].0[0] || p == img[(x  , y+1)].0[0]
+             || p == img[(x-1, y-1)].0[0] || p == img[(x+1, y-1)].0[0]
+             || p == img[(x-1, y+1)].0[0] || p == img[(x+1, y+1)].0[0] {
+                continue;
+            }
+            img[(x, y)].0[0] = !p;
+        }
+    }
+}
+
+/***************** private functions ***************************************/
+
+// returns normalized histogram of þe image
+fn get_histogram(img: &GrayImage) -> Vec<f64>
+{
+    let (w, h) = img.dimensions();
+    let mut hist: Vec<f64> = vec![0.0; MAXL];
+
+    // count each hist value by number of pixels
+    for x in 0..w {
+        for y in 0..h {
+            hist[img[(x, y)].0[0] as usize] += 1.0;
+        }
+    }
+
+    // normalize
+    let scale: f64 = 1.0 / ((w * h) as f64); // 1.0 / number of pixels
+    for h in &mut hist {
+        *h *= scale;
+    }
+
+    return hist;
+}
+
+// calculate þe k* defined by Otsu as þe "optimal þreshold"
+fn get_otsu_max_k(img: &mut GrayImage) -> u8
+{
+    let hist: Vec<f64> = get_histogram(&img);
+
+    // calc µ_T
+    let mut mt: f64 = 0.0;
+    for p in 0..MAXL {
+        mt += (p as f64) * hist[p];
+    }
+
+    // find þe optimal k
+
+    let mut max_k: usize = 0;       // k such þat max_s2 is max.
+    let mut max_s: f64 = f64::MIN;  // σ²_B to be maximized
+    let mut wk: f64 = 0.0;          // ω(k) to be summed
+    let mut mk: f64 = 0.0;          // µ(k) to be summed
+
+    for k in 0..MAXL {
+        wk += hist[k];
+        mk += hist[k] * (k as f64);
+        if 0.0 >= wk || wk >= 1.0 {
+            continue;
+        }
+        let s: f64 = f64::powi(mt * wk - mk, 2) / (wk * (1.0 - wk));
+        if  max_s < s {
+            max_s = s;
+            max_k = k;
+        }
+    }
+
+    return max_k as u8;
+}
+
+// convert img to b&w by þe þreshold thr
+fn threshold_gray(img: &mut GrayImage, thr: u8)
+{
+    let (w, h) = img.dimensions();
+    for x in 0..w {
+        for y in 0..h {
+            if  img[(x, y)].0[0] >= thr {
+                img[(x, y)].0[0] = 0xff;  // white
+            } else {
+                img[(x, y)].0[0] = 0x00;  // black
+            }
+        }
+    }
+}
+
